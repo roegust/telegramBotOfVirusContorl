237cbae297276b6836422a6914cdc35954cea53a
qdkp123456 <51738596+qdkp123456@users.noreply.github.com>
2020-03-12 10:58:59 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,22 +4,40 @@ var CronJob = require("cron").CronJob;
 const TelegramBot = require("node-telegram-bot-api");
 const token = "1062087849:AAFnz4p26ac3DsjPXEDh0fvMiahQjEZQRS8";
 const bot = new TelegramBot(token, { polling: true });
-const telegramId = 1097526124;
+const telegramId = []
+
 
 user = {
-  "10610150": "宗家榮",
-  "10511141": "黎奕宏",
-  "10602105": "盧建廷"
+  "10610150": {
+    "name": "宗家榮",
+    "telegramId": null
+  },
+  "10511141": {
+    "name": "林應凱",
+    "telegramId": null
+  },
+  "10602105": {
+    "name": "盧建廷",
+    "telegramId": null
+  },
+  "10506120": {
+    "name": "杜承浩",
+    "telegramId": null
+  }
 };
 
 // run the job everyday at 8 a.m.
 var job = new CronJob(
-  "0 8 * * *",
-  function() {
-    const req = require("./request.js");
-    req.autofill.then(res => {
-      bot.sendMessage(telegramId, res);
-    });
+  "0 8  * * *",
+  function () {
+    Object.keys(user).forEach(e => {
+      // console.log(e)
+      const req = require("./request.js");
+      req.autofill(e, user[e].name).then(res => {
+        bot.sendMessage(user[e].telegramId, res);
+      });
+    })
+
   },
   null,
   true,
@@ -28,14 +46,24 @@ var job = new CronJob(
 
 job.start();
 
-bot.onText(/\/echo (.+)/, (msg, match) => {
-  var message = match[1]
-  bot.sendMessage(msg.chat.id, message);
+bot.onText(/\/start/, (msg) => {
+  bot.sendMessage(msg.chat.id, "Please enter the empid to add yourself in daily job and check status");
+});
+
+bot.onText(/\d{8}/, (msg) => {
+  const message = msg.text
+
+  if (user[message].telegramId === null) {
+    user[message].telegramId = msg.chat.id
+    bot.sendMessage(msg.chat.id, "Add ( empid: " + message + " , name: " + user[message].name + " ) in Daily Job");
+  }
+
   if (user[message] !== undefined) {
     const req = require("./request.js");
-    req.autofill(message, encodeURI(user[message])).then(res => {
-      bot.sendMessage(msg.chat.id, res);
+    req.autofill(message, user[message].name).then(res => {
       // console.log(res)
+      bot.sendMessage(msg.chat.id, res);
     });
+    // if(telegramId.indexOf(msg.chat.id))
   }
 });
