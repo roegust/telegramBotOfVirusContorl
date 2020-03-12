b9c21aa594a82db09f96a7496db7366af15dfa65
qdkp123456 <51738596+qdkp123456@users.noreply.github.com>
2020-03-12 16:33:59 +0800

    Update index.js

--- index.js
+++ index.js
@@ -20,14 +20,16 @@ var job = new CronJob(
   "0 8  * * *",
   function () {
     
-    reloadUserList();
-    Object.keys(user).forEach(e => {
-      // console.log(e)
-      const req = require("./request.js");
-      req.autofill(e, user[e].name).then(res => {
-        bot.sendMessage(user[e].telegramId, res);
-      });
-    })
+    reloadUserList().then(() =>{
+      Object.keys(user).forEach(e => {
+        // console.log(e)
+        const req = require("./request.js");
+        req.autofill(e, user[e].name).then(res => {
+          bot.sendMessage(user[e].telegramId, res);
+        });
+      })
+    });
+    
 
   },
   null,
@@ -46,35 +48,39 @@ bot.onText(/\/start/, (msg) => {
 });
 
 bot.onText(/\/info (\d{8})/, (msg, match) => {  
-  reloadUserList();
-  if(user[match[1]] !== undefined){    
-    bot.sendMessage(msg.chat.id, `userId: ${match[1]} , name: ${user[match[1]].name} ,chatId: ${user[match[1]].telegramId}`);
-  }else{
-    bot.sendMessage(msg.chat.id, `no information of this user:  ${match[1]}`);
-    bot.sendMessage(msg.chat.id, "Please add your info at https://docs.google.com/spreadsheets/d/16ctbzOVdulA8poPSlj6SUNk50HO-Fi94aJbh8O_kvsg/edit?usp=sharing");
-    bot.sendMessage(msg.chat.id, "Your Chat ID is " + msg.chat.id);
-  }
+  reloadUserList().then(()=>{
+    if(user[match[1]] !== undefined){    
+      bot.sendMessage(msg.chat.id, `userId: ${match[1]} , name: ${user[match[1]].name} ,chatId: ${user[match[1]].telegramId}`);
+    }else{
+      bot.sendMessage(msg.chat.id, `no information of this user:  ${match[1]}`);
+      bot.sendMessage(msg.chat.id, "Please add your info at https://docs.google.com/spreadsheets/d/16ctbzOVdulA8poPSlj6SUNk50HO-Fi94aJbh8O_kvsg/edit?usp=sharing");
+      bot.sendMessage(msg.chat.id, "Your Chat ID is " + msg.chat.id);
+    }
+  });
+  
 });
 
 user
 bot.onText(/\/status (\d{8})/, (msg, match) => {
   
-  reloadUserList();
-  const message = match[1];
+  reloadUserList().then(()=>{
+    const message = match[1];
 
-  if (user[message] !== undefined && user[message].telegramId === null) {
-    user[message].telegramId = msg.chat.id
-    bot.sendMessage(msg.chat.id, "Add ( empid: " + message + " , name: " + user[message].name + " ) in Daily Job");
-  }
+    if (user[message] !== undefined && user[message].telegramId === null) {
+      user[message].telegramId = msg.chat.id
+      bot.sendMessage(msg.chat.id, "Add ( empid: " + message + " , name: " + user[message].name + " ) in Daily Job");
+    }
 
-  if (user[message] !== undefined) {
-    const req = require("./request.js");
-    req.autofill(message, user[message].name).then(res => {
-      // console.log(res)
-      bot.sendMessage(msg.chat.id, res);
-    });
-    // if(telegramId.indexOf(msg.chat.id))
-  }
+    if (user[message] !== undefined) {
+      const req = require("./request.js");
+      req.autofill(message, user[message].name).then(res => {
+        // console.log(res)
+        bot.sendMessage(msg.chat.id, res);
+      });
+      // if(telegramId.indexOf(msg.chat.id))
+    }
+  });
+  
 });
 
 async function reloadUserList(){
