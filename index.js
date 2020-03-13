c4388cc002445ead8c7c48230d3c77ee2d44f347
qdkp123456 <51738596+qdkp123456@users.noreply.github.com>
2020-03-13 16:43:06 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,21 +7,20 @@ const bot = new TelegramBot(token, { polling: true });
 
 //googlesheet setting
 
-const { GoogleSpreadsheet } = require('google-spreadsheet');
-const doc = new GoogleSpreadsheet('16ctbzOVdulA8poPSlj6SUNk50HO-Fi94aJbh8O_kvsg');
-const apikey = "AIzaSyA47786EfVYYCcALQRV5JcsvNR-YVAAKR8"
-
+const { GoogleSpreadsheet } = require("google-spreadsheet");
+const doc = new GoogleSpreadsheet(
+  "16ctbzOVdulA8poPSlj6SUNk50HO-Fi94aJbh8O_kvsg"
+);
+const apikey = "AIzaSyA47786EfVYYCcALQRV5JcsvNR-YVAAKR8";
 
 user = {};
 
 state = {};
 
-
 // run the job everyday at 8 a.m.
 var job = new CronJob(
   "0 8  * * *",
-  function () {
-
+  function() {
     reloadUserList().then(() => {
       Object.keys(user).forEach(e => {
         // console.log(e)
@@ -29,7 +28,7 @@ var job = new CronJob(
         req.autofill(e, user[e].name).then(res => {
           bot.sendMessage(user[e].telegramId, res);
         });
-      })
+      });
     });
   },
   null,
@@ -39,49 +38,71 @@ var job = new CronJob(
 
 job.start();
 
-bot.onText(/\/start/, (msg) => {
-  bot.sendMessage(msg.chat.id, `*Quick start* 
-  /info empid - check user information`
+bot.onText(/\/start/, msg => {
+  bot.sendMessage(
+    msg.chat.id,
+    `*Quick start* 
+  /info - check user information
+  /add  - add user`
+  );
 });
 
 bot.onText(/((\d{7,8}))/, (msg, match) => {
   if (state[msg.chat.id] !== undefined) {
     if (state[msg.chat.id].status === "info") {
       reloadUserList()
-      .then(() => {
-        if (user[match[1]] !== undefined) {
-          bot.sendMessage(msg.chat.id, `userId: ${match[1]} , name: ${user[match[1]].name} ,chatId: ${user[match[1]].telegramId}`);
-        } else {
-          // bot.sendMessage(msg.chat.id, `no information of this user:  ${match[1]}`);
-          bot.sendMessage(msg.chat.id, "Please add your info at https://docs.google.com/spreadsheets/d/16ctbzOVdulA8poPSlj6SUNk50HO-Fi94aJbh8O_kvsg/edit?usp=sharing");
-          bot.sendMessage(msg.chat.id, "Your Chat ID is " + msg.chat.id);
-        }
-      })
-      .then(()=>{
-        state[msg.chat.id].status = ""
-      });
+        .then(() => {
+          if (user[match[1]] !== undefined) {
+            bot.sendMessage(
+              msg.chat.id,
+              `userId: ${match[1]} , name: ${user[match[1]].name} ,chatId: ${
+                user[match[1]].telegramId
+              }`
+            );
+            bot.sendMessage(msg.chat.id, `user: ${match[1]} is already add in daily job`);
+          } else {
+             bot.sendMessage(msg.chat.id, `no information of userid:  ${match[1]}`);
+          }
+        })
+        .then(() => {
+          state[msg.chat.id].status = "";
+        });
     }
   }
 });
 
-bot.onText(/\/info/, (msg) => {
+bot.onText(/\/add/, msg => {
+  bot.sendMessage(
+    msg.chat.id,
+    "Please add your info at https://docs.google.com/spreadsheets/d/16ctbzOVdulA8poPSlj6SUNk50HO-Fi94aJbh8O_kvsg/edit?usp=sharing"
+  );
+  bot.sendMessage(msg.chat.id, "Your Chat ID is " + msg.chat.id);
+});
+
+bot.onText(/\/info/, msg => {
   if (state[msg.chat.id] === undefined) {
-    state[msg.chat.id] = stateInfo(msg.chat.id, "info")
+    state[msg.chat.id] = stateInfo(msg.chat.id, "info");
   } else {
-    state[msg.chat.id].status = "info"
+    state[msg.chat.id].status = "info";
   }
   // console.log(state)
   bot.sendMessage(msg.chat.id, "Please enter your employee ID");
 });
 
 bot.onText(/\/test (\[0-9]{8}|[0-9]{7})/, (msg, match) => {
-
   reloadUserList().then(() => {
     const message = match[1];
 
     if (user[message] !== undefined && user[message].telegramId === null) {
-      user[message].telegramId = msg.chat.id
-      bot.sendMessage(msg.chat.id, "Add ( empid: " + message + " , name: " + user[message].name + " ) in Daily Job");
+      user[message].telegramId = msg.chat.id;
+      bot.sendMessage(
+        msg.chat.id,
+        "Add ( empid: " +
+          message +
+          " , name: " +
+          user[message].name +
+          " ) in Daily Job"
+      );
     }
 
     if (user[message] !== undefined) {
@@ -93,11 +114,9 @@ bot.onText(/\/test (\[0-9]{8}|[0-9]{7})/, (msg, match) => {
       // if(telegramId.indexOf(msg.chat.id))
     }
   });
-
 });
 
 async function reloadUserList() {
-
   doc.useApiKey(apikey);
 
   await doc.loadInfo();
@@ -106,11 +125,10 @@ async function reloadUserList() {
 
   rows.forEach(e => {
     user[e.userId] = {
-      "name": e.name,
-      "telegramId": e.chatId
-    }
-  })
-
+      name: e.name,
+      telegramId: e.chatId
+    };
+  });
 }
 
 function stateInfo(chatId, type) {
