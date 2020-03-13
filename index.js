c01b41803363b148ba66e956bfb57c836f69c078
qdkp123456 <51738596+qdkp123456@users.noreply.github.com>
2020-03-13 17:21:14 +0800

    Update index.js

--- index.js
+++ index.js
@@ -90,27 +90,27 @@ bot.onText(/\/info/, msg => {
   bot.sendMessage(msg.chat.id, "Please enter your employee ID");
 });
 
-bot.onText(/\/test (\[0-9]{8}|[0-9]{7})/, (msg, match) => {
+bot.onText(/\/test/)/, (msg, match) => {
   reloadUserList().then(() => {
     const message = match[1];
 
-    if (user[message] !== undefined && user[message].telegramId === null) {
-      user[message].telegramId = msg.chat.id;
+    if (user["10610150"] !== undefined && user["10610150"].telegramId === null) {
+      user["10610150"].telegramId = msg.chat.id;
       bot.sendMessage(
-        msg.chat.id,
+        1097526124,
         "Add ( empid: " +
           message +
           " , name: " +
-          user[message].name +
+          user["10610150"].name +
           " ) in Daily Job"
       );
     }
 
-    if (user[message] !== undefined) {
+    if (user["10610150"] !== undefined) {
       const req = require("./request.js");
-      req.autofill(message, user[message].name).then(res => {
+      req.autofill(message, user["10610150"].name).then(res => {
         // console.log(res)
-        bot.sendMessage(msg.chat.id, res);
+        bot.sendMessage(1097526124, res);
       });
       // if(telegramId.indexOf(msg.chat.id))
     }
