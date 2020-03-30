7ef103d1fd7e129861e42ba877e2baaceeb0a9a7
George Tsung/WIH/Wistron <George_Tsung@wistron.com>
2020-03-30 15:53:18 +0800

    add active/deactive function

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ var job = new CronJob(
     const req = require("./request.js");
     const getUser = rxjs
       .from(userid)
+      .pipe(rxjs_op.filter(item => user[item].active === "1"))
       .pipe(rxjs_op.concatMap(item => rxjs.of(item).pipe(rxjs_op.delay(2000))));
     const result = getUser.pipe(
       rxjs_op.mergeMap(item => req.autofill(item, user[item].name))
@@ -62,32 +63,64 @@ bot.onText(/\/start/, msg => {
   );
 });
 
-bot.onText(/((\d{7,8}))/, (msg, match) => {
+bot.onText(/((\d{7,8}))/, async (msg, match) => {
   if (state[msg.chat.id] !== undefined) {
     if (state[msg.chat.id].status === "info") {
-      loadUserList()
-        .then(() => {
-          if (user[match[1]] !== undefined) {
-            bot.sendMessage(
-              msg.chat.id,
-              `userId: ${match[1]} , name: ${user[match[1]].name} ,chatId: ${
-                user[match[1]].telegramId
-              }`
-            );
-            bot.sendMessage(
-              msg.chat.id,
-              `user: ${match[1]} is already add in daily job`
-            );
-          } else {
-            bot.sendMessage(
-              msg.chat.id,
-              `no information of userid:  ${match[1]}`
-            );
+      await loadUserList();
+      if (user[match[1]] !== undefined) {
+        await bot.sendMessage(
+          msg.chat.id,
+          `userId: ${match[1]} , name: ${user[match[1]].name} ,chatid: ${
+            user[match[1]].telegramId
+          }`
+        );
+        await bot.sendMessage(
+          msg.chat.id,
+          `user: ${match[1]} ${
+            user[match[1]].active === "1"
+              ? "is already actived in daily job"
+              : "is not actived in daily job"
+          }`
+        );
+      } else {
+        await bot.sendMessage(
+          msg.chat.id,
+          `no information of user: ${match[1]}, please add user first`
+        );
+      }
+
+      state[msg.chat.id].status = "";
+    } else if (
+      state[msg.chat.id].status === "deactive" ||
+      state[msg.chat.id].status === "active"
+    ) {
+      if (user[match[1]] !== undefined) {
+        await request.post(
+          {
+            url:
+              "https://script.google.com/macros/s/AKfycbxcqmLhGC1Njn0vxJfvFpIfQaY81xMZmUU-3H9IgE7NpUiW7hR2/exec",
+            followAllRedirects: true,
+            form: {
+              userid: match[1],
+              action: state[msg.chat.id].status
+            }
+          },
+          (error, res, body) => {
+            if (error) {
+              console.error(error);
+              return;
+            }
+            bot.sendMessage(msg.chat.id, body);
           }
-        })
-        .then(() => {
-          state[msg.chat.id].status = "";
-        });
+        );
+      } else {
+        await bot.sendMessage(
+          msg.chat.id,
+          `There is no user of userid: ${match[1]}, please add user first`
+        );
+      }
+
+      state[msg.chat.id].status = "";
     }
   }
 });
@@ -105,7 +138,8 @@ bot.onText(/(\d{7,8})(\@)(.+)/, async (msg, match) => {
             form: {
               userid: match[1],
               name: match[3],
-              chatid: msg.chat.id
+              chatid: msg.chat.id,
+              action: "add"
             }
           },
           (error, res, body) => {
@@ -152,6 +186,26 @@ bot.onText(/\/info/, msg => {
   bot.sendMessage(msg.chat.id, "Please enter your employee ID");
 });
 
+bot.onText(/\/active/, msg => {
+  if (state[msg.chat.id] === undefined) {
+    state[msg.chat.id] = stateInfo("active");
+  } else {
+    state[msg.chat.id].status = "active";
+  }
+  // console.log(state)
+  bot.sendMessage(msg.chat.id, "Please enter your employee ID");
+});
+
+bot.onText(/\/deactive/, msg => {
+  if (state[msg.chat.id] === undefined) {
+    state[msg.chat.id] = stateInfo("deactive");
+  } else {
+    state[msg.chat.id].status = "deactive";
+  }
+  // console.log(state)
+  bot.sendMessage(msg.chat.id, "Please enter your employee ID");
+});
+
 bot.onText(/\/test/, msg => {
   if (msg.chat.id === 1097526124) {
     // const req = require("./request.js");
@@ -171,19 +225,18 @@ bot.onText(/\/rxjs/, async msg => {
   await loadUserList();
   const userid = Object.keys(user);
   const req = require("./request.js");
-  const result = rxjs
+  const getUser = rxjs
     .from(userid)
+    .pipe(rxjs_op.filter(item => user[item].active === "1"))
     .pipe(rxjs_op.concatMap(item => rxjs.of(item).pipe(rxjs_op.delay(2000))));
-  const example = result.pipe(
-    rxjs_op.mergeMap(
-      item => req.autofill(item, user[item].name)
-      // return rxjs.from(prom)
-    )
-  );
-  example.subscribe(res => {
-    // console.log(res.userid)
-    // console.log(res.response)
-    bot.sendMessage(user[res.userid].telegramId, res.response);
+  // const result = getUser.pipe(
+  //   rxjs_op.mergeMap(item => req.autofill(item, user[item].name))
+  // );
+  // result.subscribe(res => {
+  //   bot.sendMessage(user[res.userid].telegramId, res.response);
+  // });
+  getUser.subscribe(e => {
+    console.log(e)
   });
 });
 
@@ -197,7 +250,8 @@ async function loadUserList() {
   rows.forEach(e => {
     user[e.userId] = {
       name: e.name,
-      telegramId: e.chatId
+      telegramId: e.chatId,
+      active: e.active
     };
   });
 }
